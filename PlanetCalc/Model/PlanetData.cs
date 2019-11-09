3f9c60e974223c1d2c4d1374b276ce02ddd7da51
blackpointdev <marcinw1998@gmail.com>
2019-11-09 19:58:39 +0100

    Added UI controls binding, fixes for window settings.

--- PlanetCalc/Model/PlanetData.cs
+++ PlanetCalc/Model/PlanetData.cs
@@ -14,15 +14,15 @@ namespace PlanetCalc.Model
 
             planets.Add(new Planet(
                                     "Merkury",
-                                    2.4397E6,
+                                    2.4397E3,
                                     3.285E23));
             planets.Add(new Planet(
                                     "Wenus",
-                                    6.0518E6,
+                                    6.0518E3,
                                     4.867E24));
             planets.Add(new Planet(
                                     "Ziemia",
-                                    6.371E6,
+                                    6.371E3,
                                     5.972E24));
             return planets;
         }
