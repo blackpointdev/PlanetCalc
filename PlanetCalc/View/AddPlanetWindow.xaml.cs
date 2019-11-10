42f868a03b71849b8db7af5df288dac7c7dd190e
blackpointdev <marcinw1998@gmail.com>
2019-11-10 01:56:47 +0100

    Added missing files.

---
+++ PlanetCalc/View/AddPlanetWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using PlanetCalc.Model;
+using PlanetCalc.ViewModel;
+
+namespace PlanetCalc.View
+{
+    /// <summary>
+    /// Interaction logic for AddPlanetWindow.xaml
+    /// </summary>
+    public partial class AddPlanetWindow : Window
+    {
+        private MainWindowModel mainWindowModel;
+        public AddPlanetWindow(ref MainWindowModel viewModel)
+        {
+            InitializeComponent();
+            mainWindowModel = viewModel;
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            string name = NameInput.Text;
+            double radius;
+            double mass;
+
+            double.TryParse(RadiusInput.Text, out radius);
+            double.TryParse(MassInput.Text, out mass);
+            //TODO Data validation
+
+            Planet planet = new Planet(name, radius, mass);
+            mainWindowModel.Planets.Add(planet);
+
+            //TODO This db path passing is veery wrong.
+            DBConnection db = new DBConnection("PlanetsRepository.db");
+            db.AddPlanet(planet);
+
+            this.Close();
+        }
+    }
+}
